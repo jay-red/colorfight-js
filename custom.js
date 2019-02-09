d3ea1e9fa78e5734fcf5605c4965b3431439c091
Jared Flores <jared_flores@umail.ucsb.edu>
2019-02-08 18:57:49 -0800

    Update custom.js

--- custom.js
+++ custom.js
@@ -10,7 +10,7 @@ var token = "",
 	mode = "",
 	attackIndex = 0,
 	blastIndex = 0,
-	host = "https://colorfight.herokuapp.com/";
+	host = "http://colorfight.herokuapp.com/";
 
 function randomize( array ) {
     var currentIndex = array.length,
